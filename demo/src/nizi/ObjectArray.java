4b31fe0a1e1b5818cb982dcc038d15582cedf7bc
acevv <acev_aleksandar@hotmail.com>
2021-09-22 11:03:20 +0200

    objectArray

---
+++ demo/src/nizi/ObjectArray.java
@@ -0,0 +1,22 @@
+package nizi;
+
+public class ObjectArray {
+
+	public static void main(String[] args) {
+
+		Account obj[] = new Account[2];
+		obj[0] = new Account();
+		obj[1] = new Account();
+
+		obj[0].setData(1, 2);
+		obj[1].setData(3, 4);
+
+		System.out.println("For array element 0");
+		obj[0].showData();
+
+		System.out.println("For array element 1");
+		obj[1].showData();
+
+	}
+
+}
