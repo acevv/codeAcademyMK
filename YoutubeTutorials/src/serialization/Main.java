d63799ef74bce56bc13c3418c0fa0bd45f4dbad4
acevv <acev_aleksandar@hotmail.com>
2021-10-28 19:03:54 +0200

    serialization

---
+++ YoutubeTutorials/src/serialization/Main.java
@@ -0,0 +1,40 @@
+package serialization;
+
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+//		 Serialization = The process of converting an object into a byte stream.
+		// Persists (saves the state) the object after program exits
+		// This byte stream can be saved as a file or sent over a network
+		// Byte stream can be saved as a file (.ser) which is platform independent
+		// (Think of this as if you're saving a file with the object's information)
+
+//		Steps to Serialize
+		// ---------------------------------------------------------------
+		// 1. Your class should implement Serializable interface
+		// 2. add import java.io.Serializable;
+		// 3. FileOutputStream fileOut = new FileOutputStream(file path)
+		// 4. ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		// 5. out.writeObject(objectName)
+		// 6. out.close(); fileOut.close();
+		// ---------------------------------------------------------------
+
+		User user = new User();
+
+		user.name = "acev";
+		user.password = "pasvord";
+
+		FileOutputStream fileOut = new FileOutputStream("UserInfo.ser");
+		ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+		out.writeObject(user);
+		out.close();
+		fileOut.close();
+
+		System.out.println("Object info saved!");
+	}
+
+}
