3bec735bc3099679458df6bd29a45c66191198ec
acevv <acev_aleksandar@hotmail.com>
2021-12-01 20:57:06 +0100

    method fix

--- dbConn/src/main/java/org/test/dbConn/UsersDAOImpl.java
+++ dbConn/src/main/java/org/test/dbConn/UsersDAOImpl.java
@@ -42,7 +42,7 @@ public class UsersDAOImpl implements UsersDAO {
 
 			ResultSet rs = stmt.executeQuery(query);
 
-			if (rs.next()) {
+			while (rs.next()) {
 				Korisnik user = new Korisnik();
 				user = extractUserFromResultSet(rs);
 				users.add(user);
@@ -88,29 +88,27 @@ public class UsersDAOImpl implements UsersDAO {
 	public String insertUserInDBwithPreparedStatement(Korisnik user) {
 		Connection conn = JDBCConfig.getConnection();
 		String result = "";
-		
+
 		try {
-			String query = "INSERT INTO public.korisnik(id, name, email, user_pass)\n"
-					+ "	VALUES (?, ?, ?, ?);";
+			String query = "INSERT INTO public.korisnik(id, name, email, user_pass)\n" + "	VALUES (?, ?, ?, ?);";
 			PreparedStatement ps = conn.prepareStatement(query);
 			ps.setInt(1, user.getId());
 			ps.setString(2, user.getName());
 			ps.setString(3, user.getEmail());
 			ps.setString(4, user.getUser_pass());
-			
+
 			int status = ps.executeUpdate();
-			
+
 			if (status == 1) {
 				result = "Successfully inserted.";
 			} else {
 				result = "Record has not been inserted.";
 			}
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
 }
